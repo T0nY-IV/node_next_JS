6780510a88c757223bdc790dd7542a258b609d70
Yessineht <yessineht@gmail.com>
2025-05-07 01:03:44 +0100

    configure user model and user route

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+const userSchema = new mongoose.Schema({
+    Cin : { type: String, required: true, primary: true },
+    Nom : { type: String, required: true },
+    Prenom : { type: String, required: true },
+    Email : { type: String, required: true },
+    Password : { type: String, required: true },
+    Role : { type: String, required: true, enum: ['admin', 'user'] },
+})
+module.exports = mongoose.model("User", userSchema);
